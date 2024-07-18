6509c1fe4d5d3ba6b95a3656afe3aa485a213db8
Valeriy Aksonov <vaksonov@brita.net>
2024-07-18 18:44:37 +0200

    solutions 06-11

--- src/11-record.problem.ts
+++ src/11-record.problem.ts
@@ -1,7 +1,8 @@
 import { expect, it } from "vitest";
 
 const createCache = () => {
-  const cache = {};
+  // const cache: Record<string, string> = {};
+  const cache: { [id: string]: string; } = {};
 
   const add = (id: string, value: string) => {
     cache[id] = value;
